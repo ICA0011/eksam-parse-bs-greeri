b1354a2e6cbf477a9db0f09d99741eb5563c1aff
grete eerikson <92260129+greeri@users.noreply.github.com>
2022-05-26 12:53:36 +0300

    Update parse_xml.py

--- parse_xml.py
+++ parse_xml.py
@@ -1,12 +1,16 @@
 from bs4 import BeautifulSoup
 import requests
-import lxml
+
 
 def parse_xml():
-  url = "http://upload.itcollege.ee/~aleksei/test.xml"
-  xml_content = requests.get(url).content
-  soup = BeautifulSoup(xml_content,'xml')
-  
-  # your code here
-  
-  return result
+    url = "http://upload.itcollege.ee/~aleksei/test.xml"
+    xml_content = requests.get(url).content
+    soup = BeautifulSoup(xml_content, 'xml')
+    titles = soup.find_all('data')
+
+    for title in titles:
+        answer = title.get_text()
+
+    return str(answer.strip())
+
+print(parse_xml())
